967368d7577a9b91f4dc5b42ca0c88970f439825
rlb77 <rlb@rose.net>
2017-06-19 11:24:54 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Rich
+# Date: 6/19/17
 
 # proj01: A Simple Program
 # This program asks the user for his/her name and age.
@@ -7,4 +7,15 @@
 
 # If you complete extensions, describe your extensions here!
 
-
+User_Name = raw_input("What is your Name?:  ")
+firstletter = User_Name[0]
+cf = firstletter.upper()
+op = User_Name[1:].lower()
+print "Your name is", cf + op
+User_Age = int(raw_input("How old are you?:  "))
+x = ((100-User_Age)+2017)
+BD = raw_input("Have you had your birthday this year?:  ")
+y = x
+if (BD == "no"): y = x - 1
+print "You will be 100 in", y
+if User_Age < 17:
