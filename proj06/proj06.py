4fde06af36281aa22f7abc14745d24fc1029a1c0
rlb77 <rlb@rose.net>
2017-06-22 13:15:38 -0500

    Add files via upload

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -1,64 +1,49 @@
-word_in_list = ['d', 'o', 'o', 'o', 'o', 'o','t']
-guess_letter = raw_input('Enter a letter')
+word_in_list = ['d', 'o', 'o', 'o', 'o', 'o','t', 't']
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-guesses = 8
+guesses = 1
 wordlength = int(len(word_in_list))
 alphabetlength = int(len(alphabet))
 y = 0
 z = 0
 final_word_reveal = []
-for var in range(wordlength):
-    if guess_letter == word_in_list[y]:
-        print "\nGreat guess!"
-        word_in_list.remove(str(guess_letter))
-        final_word_reveal.append(str(guess_letter))
-    else:
-        print 'none'
-        y = y + 1
-    if word_in_list == []:
-        print "\nYou finished the game! Hooray!"
-
-for var2 in range(alphabetlength):
-    if guess_letter == alphabet[z]:
-        alphabet.remove(str(guess_letter))
-    else:
-        z = z + 1
-
-y = 0
-z = 0
-wordlength = int(len(word_in_list))
-alphabetlength = int(len(alphabet))
-print "Available letters: " + str(alphabet)
-print word_in_list
-
-
-guess_letter = raw_input('Enter a number')
 
 
 
-for var in range(wordlength):
-    if guess_letter == word_in_list[y]:
-        print "\nGreat guess!"
-        word_in_list.remove(str(guess_letter))
-        final_word_reveal.append(str(guess_letter))
-    else:
-        print 'none'
-        y = y + 1
-    if word_in_list == []:
-        print "\nYou finished the game! Hooray!"
 
-for var2 in range(alphabetlength):
-    if guess_letter == alphabet[z]:
-        alphabet.remove(str(guess_letter))
-    else:
-        z = z + 1
 
-y = 0
-z = 0
-wordlength = int(len(word_in_list))
-alphabetlength = int(len(alphabet))
-print "Available letters: " + str(alphabet)
-print word_in_list
+
+while word_in_list != []:
+    guess_letter = raw_input('Guess a letter')
+    for var in range(wordlength):
+        if guess_letter == word_in_list[y]:
+            print "\nGreat guess!"
+            word_in_list.remove(str(guess_letter))
+            final_word_reveal.append(str(guess_letter))
+        else:
+            y = y + 1
+        if word_in_list == []:
+            print "\nYou finished the game! Hooray!"
+    for var2 in range(alphabetlength):
+        if guess_letter == alphabet[z]:
+            alphabet.remove(str(guess_letter))
+        else:
+            z = z + 1
+    if guesses == 8:
+        print 'gameover'
+        word_in_list = []
+    guesses = guesses + 1
+    print guesses
+
+
+    y = 0
+    z = 0
+    wordlength = int(len(word_in_list))
+    alphabetlength = int(len(alphabet))
+    print "Available letters: " + str(alphabet)
+    print word_in_list
+
+
+
 
 
 # list = ['a', 'b', 'c', 'd', 'e', 'f' ,'g', 'g', 'g', 'a', 'a', 'b']
